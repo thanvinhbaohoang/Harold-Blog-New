666cdbf5db1f065ff2c53c630e09a75d07b7e871
Harold <harold@pop-os.localdomain>
2023-07-03 16:59:52 -0400

    Added Tailwind Typography with Style

--- src/Pages/BlogPage.tsx
+++ src/Pages/BlogPage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import ReactMarkdown from 'react-markdown'
 
 export default function BlogPage() {
     return (
-        <article class="">
+    <article class="">
           <header class="mb-4 lg:mb-6 not-format">
               <address class="flex items-center mb-6 not-italic">
                   <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
@@ -15,25 +15,27 @@ export default function BlogPage() {
                       </div>
                   </div>
               </address>
-              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">Best practices for successful prototypes</h1>
           </header>
 
+          <div class="prose-invert mx-auto max-w-none prose-p:text-gray-400 prose-headings:font-bold prose-xl w-full prose-teal ">
+          <article>
 
+            <h1> Example Bullshit Title For My Blog</h1>
+<h2>Google Chrome</h2>
+<p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+            </article>
 
-        <p>
+            <article>
+            <h2>Mozilla Firefox</h2>
+            <p>Mozilla Firefox is an open-source web browser developed by Mozilla. Firefox has been the second most popular web browser since January, 2018.</p>
+            </article>
 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh justo, consequat sed lorem nec, hendrerit lobortis tortor. Integer hendrerit nunc sit amet volutpat finibus. Nulla sem eros, varius non lobortis non, lobortis id lectus. Fusce nec libero non orci ullamcorper aliquet. Sed non mauris sit amet neque euismod elementum. Proin at molestie nisi, vel aliquam leo. Etiam ultrices ligula at ultrices elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed quis cursus dolor, quis lacinia sem. Nulla at pulvinar lectus. Cras ut nunc nibh. Praesent in libero eget odio fermentum varius sit amet sit amet leo. Vivamus nec quam sed tellus consectetur interdum vitae eget nulla. Nullam vel ligula ac ligula aliquam dignissim consequat ac dui. Ut lorem nibh, dictum ac diam vitae, aliquet posuere justo.
+            <article>
+            <h2>Microsoft Edge</h2>
+            <p>Microsoft Edge is a web browser developed by Microsoft, released in 2015. Microsoft Edge replaced Internet Explorer.</p>
+            </article>
+        </div>
 
-Suspendisse auctor ex purus. Etiam et volutpat purus. In sapien velit, rhoncus a posuere ac, pharetra id orci. Aliquam efficitur libero ligula, vitae dictum lorem consequat id. Duis ultricies placerat risus, eleifend tincidunt eros fringilla eu. Sed pulvinar interdum sodales. Nunc pellentesque mi id enim mattis varius. Curabitur eu dui urna. Morbi magna lacus, varius ut molestie eu, consectetur eget ipsum. Aenean in dui molestie mauris posuere lacinia ut vitae orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum hendrerit mollis massa a eleifend. Nulla eget libero vulputate, elementum urna id, fringilla nunc. Aenean et ultrices metus, at rutrum risus. Ut sit amet lobortis lorem.
-
-Sed pharetra pharetra convallis. Nullam maximus eget nibh quis faucibus. Maecenas quis dictum arcu. Pellentesque eu justo eget ligula iaculis vulputate a et arcu. Etiam quis vestibulum nisl. Nam ut neque volutpat, viverra sapien at, accumsan arcu. Fusce sed augue eleifend, hendrerit neque a, consectetur dui. Proin venenatis hendrerit porttitor. Etiam egestas varius tortor in ultrices. Vestibulum gravida metus vel purus ornare rutrum. Morbi in dictum mauris, non aliquam velit. Morbi eu enim quam. Donec iaculis dui eu risus placerat, in suscipit turpis ullamcorper.
-
-Donec tincidunt sem et neque lacinia iaculis. Nunc sit amet pretium mauris. Curabitur rutrum dignissim sapien et bibendum. Quisque eleifend suscipit ullamcorper. Sed vel libero risus. Morbi sollicitudin ac felis quis ullamcorper. In nibh eros, efficitur vitae ex at, gravida venenatis lacus. Nunc lectus ipsum, bibendum a volutpat vel, bibendum nec ipsum. Phasellus iaculis commodo scelerisque. Ut maximus, tortor in suscipit vulputate, arcu dui blandit turpis, eu imperdiet neque ante a metus. Aenean a tortor ut lectus hendrerit blandit.
-
-Praesent faucibus lectus eget neque aliquet, in rutrum magna tristique. Sed congue, dolor nec faucibus rhoncus, ligula mi dictum leo, vel eleifend felis urna sit amet nulla. Donec tincidunt accumsan mauris ut fermentum. In hac habitasse platea dictumst. Proin non lorem malesuada, laoreet turpis id, tincidunt sem. In semper, elit id feugiat eleifend, dui mauris suscipit turpis, ac ullamcorper orci lorem eget ante. Etiam cursus arcu sit amet ex interdum, vel euismod justo tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus imperdiet, enim sodales porttitor interdum, tortor purus maximus est, sit amet consequat magna eros sit amet tellus. Duis at enim pharetra, interdum augue vel, iaculis sapien. Ut mattis purus velit, a tempus magna ullamcorper quis.
-
-
-        </p>
         </article>
     )
 }
